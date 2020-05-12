ae7b0ab3e20c9aa30a56b6c1df0283be172c51c5
邱高强 <qiugaoqiang>
2020-05-12 14:50:06 +0800

    第一次提交

---
+++ src/main/java/com/bigjava/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.bigjava.git;
+
+/**
+ * @ClassName : FirstClass
+ * @Description : 测试Git版本控制工具
+ * @Author : 邱高强
+ * @Date: 2020-05-12 14:41
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git project");
+    }
+}
